10ab6d7d43cbaba992a484eb92e1f342bfdccffd
huizheng <1334295412@qq.com>
2022-02-22 09:41:47 +0800

    加上点击动画

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -20,8 +20,8 @@ import {
   ImageBackground,
   PanResponder,
   Animated,
-  useWindowDimensions, 
-  Easing
+  useWindowDimensions,
+  Easing,
 } from 'react-native'
 
 import {
@@ -40,11 +40,8 @@ import base2x from './image/base2x.png'
 import whiteBg from './image/whiteBg.png'
 import array from './AppData'
 const totalH = 266
-import {BoxShadow} from 'react-native-shadow'// 阴影
-import LinearGradient from 'react-native-linear-gradient'// 渐变
-
-
-
+import {BoxShadow} from 'react-native-shadow' // 阴影
+import LinearGradient from 'react-native-linear-gradient' // 渐变
 
 const panResponderTemp = PanResponder.create({
   onStartShouldSetPanResponder: () => true,
@@ -76,51 +73,81 @@ function BoxShadowItem (props) {
     width: everyLineW + 3,
     height: item.style[1].height,
     color: '#000',
-    radius: 10,
+    radius: 20,
     opacity: 0.15,
-    border: 4,
-    x: 2,
+    border: 3,
+    x: 0,
     y: 4,
   }
   let css = [styles.line_box, ...item.style]
   css.push(styles.line_box_hover)
   css.push({width: everyLineW + 3})
-  css = css.concat(item.hover)
+  css.push(item.hover)
+  const [scale, setScale] = useState(new Animated.Value(1));
+  // 模拟DidMount
+  useEffect(() => {
+    // transform: [{scale: 1.05}]
+    startAni(item)
+  }, [])
+  const startAni = (item) => {
+    
+    console.log('startAni', props)
+    Animated.timing(scale, {
+      toValue: 1.05,
+      duration: 300,
+      delay: 0,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }).start(() => {
+      // callback
+      
+    })
+  }
   return (
+    <Animated.View style={{transform:[{scale: scale}]}}>
     <BoxShadow setting={shadowOpt}>
-      <View style={css}>
+      <LinearGradient colors={item.hoverColor} style={css}>
         <Text style={styles.score_box}>{item.y}</Text>
         <Image source={item.img} style={styles.face_box} />
-      </View>
+      </LinearGradient>
     </BoxShadow>
+    </Animated.View>
   )
 }
 class UnBoxShadowItem extends React.Component {
-  constructor() {
-      super();
-      this.state = {
-          height: new Animated.Value(65),
-          opacity: new Animated.Value(0.2),
-      };
-  }
-  componentDidMount(){
-    const item = this.props.item;
-    const style = item.style;
-    if(!item.AnimatedFinish){
-      this.startAni();
-    }else{
-      this.setState({height: style[1].height});
-      this.setState({opacity: 1});
+  constructor () {
+    super()
+    this.state = {
+      height: new Animated.Value(65),
+      opacity: new Animated.Value(0.2),
     }
-    
   }
-  render() {
-    const props = this.props;
+  componentDidMount () {
+    const item = this.props.item
+    const style = item.style
+    if (!item.AnimatedFinish) {
+      this.startAni()
+    } else {
+      this.setState({height: style[1].height})
+      this.setState({opacity: 1})
+    }
+  }
+  render () {
+    const props = this.props
     const item = props.item
     const everyLineW = props.everyLineW
 
     return (
-      <Animated.View style={[styles.line_box, ...item.style, {width: everyLineW, height: this.state.height, opacity: this.state.opacity}]}>
+      <Animated.View
+        style={[
+          styles.line_box,
+          ...item.style,
+          {
+            width: everyLineW,
+            height: this.state.height,
+            opacity: this.state.opacity,
+          },
+        ]}>
         <Text style={styles.score_box}>{item.y}</Text>
         <Image source={item.img} style={styles.face_box} />
       </Animated.View>
@@ -128,51 +155,60 @@ class UnBoxShadowItem extends React.Component {
   }
 
   startAni = () => {
-    const props = this.props;
+    const props = this.props
     const item = props.item
-    console.log('startAni',props)
-    Animated.parallel([ 
-      Animated.timing(this.state.height, {
-          toValue:  item.style[1].height,
-          duration: 100*(item.index + 1),
-          delay: 42*(item.index + 1),
-          easing: Easing.ease,
-          useNativeDriver: false
-      }), 
-      Animated.timing(this.state.opacity, { 
-        toValue: 1, 
-        duration: 82*(item.index+1),
-        delay: 42*item.index,
-        easing: Easing.ease,
-        useNativeDriver: false
-      }) 
-    ],{
-      stopTogether: false
-    }).start(() => { 
-        // callback 
-        item.AnimatedFinish = true;
-    }); 
-  };
+    console.log('startAni', props)
+    Animated.parallel(
+      [
+        Animated.timing(this.state.height, {
+          toValue: item.style[1].height,
+          duration: 142 + 42 * (item.index + 1),
+          delay: 8 * (item.index + 1),
+          easing: Easing.linear,
+          useNativeDriver: false,
+        }),
+        Animated.timing(this.state.opacity, {
+          toValue: 1,
+          duration: 42 * (item.index + 1),
+          delay: 8 * item.index,
+          easing: Easing.linear,
+          useNativeDriver: false,
+        }),
+      ],
+      {
+        stopTogether: false,
+      },
+    ).start(() => {
+      // callback
+      item.AnimatedFinish = true
+    })
+  }
 }
-function WeekDayText(props){
-  const item = props.item;
-  const day = props.day;
+function WeekDayText (props) {
+  const item = props.item
+  const day = props.day
   const clickIndex = props.clickIndex
   return (
-    <Text style={item.day === day ? styles.week_day_color: ''}>{item.x}</Text>
-  );
+    <Text style={item.day === day ? styles.week_day_color : ''}>{item.x}</Text>
+  )
 }
-function WeekDayTextHover(props){
-  const item = props.item;
-  const day = props.day;
-  return(
-    <ImageBackground source={whiteBg} style={[{ width: 56,
-    height: 56}]}>
-      <Text style={[styles.week_day_color_hover,{ position: "absolute",
-    bottom: 20,
-    left: "50%",
-    transform: [{translateX: -8}]
-    }]}>{item.x}</Text>
+function WeekDayTextHover (props) {
+  const item = props.item
+  const day = props.day
+  return (
+    <ImageBackground source={whiteBg} style={[{width: 56, height: 56}]}>
+      <Text
+        style={[
+          item.week_day_color_hover,
+          {
+            position: 'absolute',
+            bottom: 22,
+            left: '50%',
+            transform: [{translateX: -8}],
+          },
+        ]}>
+        {item.x}
+      </Text>
     </ImageBackground>
   )
 }
@@ -181,29 +217,38 @@ function Listitem (props) {
   const item = props.item
   // const refresh = props.refresh;
   const clickIndex = props.clickIndex
+  const marginLeft = item.index === 0 ? 13 : 6;
+  const marginBottom = item.index === (array.length - 1) ? 13 : 6;
 
   function curHandleClick (item) {
     props.handleClick(item)
   }
-  const day = props.day;
+  const day = props.day
   return (
     <View
-      style={styles.week_box}
+      style={[styles.week_box, {marginLeft: marginLeft}]}
       onTouchStart={e => console.log('start')}
       onTouchMove={e => console.log('move')}
       onTouchEnd={e => curHandleClick(item)}>
       {item.clicked || clickIndex === item.index ? (
-        <BoxShadowItem item={item} everyLineW={props.everyLineW}/>
+        <BoxShadowItem item={item} everyLineW={props.everyLineW} />
       ) : (
-        <UnBoxShadowItem item={item} everyLineW={props.everyLineW}/>
+        <UnBoxShadowItem item={item} everyLineW={props.everyLineW} />
       )}
-      <View style={[styles.week_day, item.clicked || clickIndex === item.index ?'':(item.day === day ? styles.week_day_hover:'')]}>
-        {item.clicked || clickIndex === item.index ?(
-          <WeekDayTextHover item={item} day={day}/>
-        ):(
-          <WeekDayText item={item} day={day}/>
+      <View
+        style={[
+          styles.week_day,
+          item.clicked || clickIndex === item.index
+            ? ''
+            : (item.day === day
+            ? styles.week_day_hover
+            : ''),
+        ]}>
+        {item.clicked || clickIndex === item.index ? (
+          <WeekDayTextHover item={item} day={day} />
+        ) : (
+          <WeekDayText item={item} day={day} />
         )}
-        
       </View>
     </View>
   )
@@ -214,15 +259,21 @@ function List (props) {
   arraySrc.forEach((val, index) => {
     if (val.y < 50) {
       val.style = [styles.low, {height: (val.y / 100) * totalH}]
-      val.hover = [styles.hover_low]
+      val.hoverColor = ['#CFCFCF', '#CFCFCF']
+      val.hover = styles.hover_low
+      val.week_day_color_hover = styles.week_day_color_hover_low
       val.img = question2x
     } else if (val.y < 90) {
       val.style = [styles.good, {height: (val.y / 100) * totalH}]
-      val.hover = [styles.hover_good]
+      val.hoverColor = ['#42F373', '#A1FD44']
+      val.hover = styles.hover_good
+      val.week_day_color_hover = styles.week_day_color_hover_good
       val.img = greenFace2x
     } else {
       val.style = [styles.high, {height: (val.y / 100) * totalH}]
-      val.hover = [styles.hover_high]
+      val.hoverColor = ['#FFA14A', '#FFCC4A']
+      val.hover = styles.hover_high
+      val.week_day_color_hover = styles.week_day_color_hover_high
       val.img = orangeFace2x
     }
     val.clicked = false
@@ -247,7 +298,7 @@ function List (props) {
   }
 
   let date = new Date()
-  let day = date.getDay();
+  let day = date.getDay()
 
   const listItems = array.map(item => (
     <Listitem
@@ -262,38 +313,47 @@ function List (props) {
   return <View style={[styles.app]}>{listItems}</View>
 }
 function UserInfo (props) {
-  const windowHeight = props.windowHeight;
-  const userH = windowHeight - 336 - 88 - 20;// 整个用户信息名片的高度
+  const windowHeight = props.windowHeight
+  const userH = windowHeight - 336 - 88 - 20 // 整个用户信息名片的高度
   // 分为上间距：43，头像：42，分数：98，标签：25，下间距：34
-  const designH = 43 + 42 + 98 + 25 + 34;
-  const iconTopH = userH*(43/designH)
-  const iconH = userH*(42/designH)
-  const scoreH = userH*(98/designH)
-  const scoreFontSize = userH*(72/designH)
-  const labelH = userH*(25/designH) + userH*(26/designH)
-  console.log(iconTopH,iconH,scoreH,scoreFontSize,labelH)
+  const designH = 43 + 42 + 98 + 25 + 34
+  const iconTopH = userH * (43 / designH)
+  const iconH = userH * (42 / designH)
+  const scoreH = userH * (98 / designH)
+  const scoreFontSize = userH * (72 / designH)
+  const labelH = userH * (25 / designH) + userH * (26 / designH)
+  console.log(iconTopH, iconH, scoreH, scoreFontSize, labelH)
   return (
-
-      <View style={[styles.user_info,{height: userH}]}>
-        <View style={[styles.first_line,{marginTop: iconTopH}]}>
-          <View style={styles.space}>
-            <Image source={userIcon} style={[styles.user_icon, {height: iconH, width: iconH}]} />
-          </View>
-          <Text style={styles.user_name}>李强</Text>
+    <View style={[styles.user_info, {height: userH}]}>
+      <View style={[styles.first_line, {marginTop: iconTopH}]}>
+        <View style={styles.space}>
+          <Image
+            source={userIcon}
+            style={[styles.user_icon, {height: iconH, width: iconH}]}
+          />
         </View>
-        <Text style={[styles.second_line,{height: scoreH, lineHeight: scoreH, fontSize: scoreFontSize}]}>88</Text>
-        <Text style={[styles.third_line,{height: labelH, lineHeight: labelH}]}>周平均心情指数</Text>
+        <Text style={styles.user_name}>李强</Text>
       </View>
-  
+      <Text
+        style={[
+          styles.second_line,
+          {height: scoreH, lineHeight: scoreH, fontSize: scoreFontSize},
+        ]}>
+        88
+      </Text>
+      <Text style={[styles.third_line, {height: labelH, lineHeight: labelH}]}>
+        周平均心情指数
+      </Text>
+    </View>
   )
 }
-function Nav(prop){
+function Nav (prop) {
   return (
-    <View style={styles.nav}> 
+    <View style={styles.nav}>
       <Image source={Vector_28} style={styles.nav_img} />
-      <Text  style={styles.nav_title}>历史心情指数</Text>
+      <Text style={styles.nav_title}>历史心情指数</Text>
     </View>
-  );
+  )
 }
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
@@ -301,40 +361,23 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   }
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
-  const everyLineW = windowWidth/7 - 12;
-  
+  const windowWidth = useWindowDimensions().width
+  const windowHeight = useWindowDimensions().height
+  const everyLineW = windowWidth / 7 - 14
+
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Nav />
-      
-      <ScrollView >
-        <UserInfo windowHeight={windowHeight}/>
-        <List array={array} everyLineW={everyLineW}/>
-        <LinearGradient colors={['#FFD801', '#FF8040', '#F75D59']} style={styles.linearGradient}>
-          <Text style={{color:'#fff'}}>
-            Sign in with Facebook
-          </Text>
-        </LinearGradient>
-
-      
+      <ScrollView>
+        <UserInfo windowHeight={windowHeight} />
+        <List array={array} everyLineW={everyLineW} />
       </ScrollView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  linearGradient: {
-    justifyContent:'center',
-    alignItems:'center',
-    width:200,
-    height:50,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 5
-  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -479,7 +522,7 @@ const styles = StyleSheet.create({
     marginRight: 6,
     marginTop: 50,
     height: 288,
-    marginBottom: 100
+    marginBottom: 100,
   },
 
   week_day: {
@@ -495,7 +538,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     letterSpacing: -0.3,
-    marginTop: 17,
+    marginTop: 32,
     /* Function 33 */
   },
   week_day_hover: {
@@ -505,13 +548,24 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     /* cate green */
     color: 'white',
+    marginTop: 18,
+    top: 3
   },
   week_day_color: {
-    color: 'white'
+    color: 'white',
   },
-  week_day_color_hover: {
+  week_day_color_hover_low: {
+    borderRadius: 8,
+    color: '#2D2F33'
+  },
+  week_day_color_hover_good: {
     borderRadius: 8,
     color: '#52C873'
+    /* #F36A1B*/
+  },
+  week_day_color_hover_high: {
+    borderRadius: 8,
+    color: '#F36A1B'
     /* #F36A1B*/
   },
   low: {
@@ -530,30 +584,36 @@ const styles = StyleSheet.create({
     borderColor: '#DCFFD6',
   },
   hover_low: {
-    backgroundColor: '#CFCFCF',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderColor: '#F2F2F2',
   },
   hover_good: {
-    backgroundColor: '#A1FD44',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderColor: '#DCFFD6',
   },
   hover_high: {
-    backgroundColor: '#FFCC4A',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderColor: '#FFE9D5',
   },
 
-  user_info: { 
+  user_info: {
     width: 390,
     height: 399,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent:'center',
+    justifyContent: 'center',
     backgroundColor: '#FFFFFF',
-    borderRadius: 24
+    borderRadius: 24,
   },
   space: {
     flex: 1,
     display: 'flex',
     alignItems: 'flex-end',
-    justifyContent:'center',
+    justifyContent: 'center',
   },
   first_line: {
     display: 'flex',
@@ -561,10 +621,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     marginTop: 44,
-    background:`url(${base2x})`
+    background: `url(${base2x})`,
   },
   user_icon: {
-    
     width: 80,
     height: 80,
     borderWidth: 1,
@@ -596,10 +655,10 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     letterSpacing: -0.3,
     color: 'rgba(45, 47, 51, 1)',
-    flex: 1
+    flex: 1,
   },
   third_line: {
-    flex:1,
+    flex: 1,
     height: 25,
     fontFamily: 'PingFang HK',
     fontStyle: 'normal',
@@ -613,19 +672,19 @@ const styles = StyleSheet.create({
 
     /* Function ae */
 
-    color: 'rgba(146, 146, 146, 1)'
+    color: 'rgba(146, 146, 146, 1)',
   },
   nav: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 88
+    height: 88,
   },
   nav_img: {
     position: 'absolute',
     width: 23.5,
     height: 17,
-    left: 19
+    left: 19,
   },
   nav_title: {
     fontFamily: 'PingFang HK',
@@ -634,16 +693,16 @@ const styles = StyleSheet.create({
     fontSize: 20,
     lineHeight: 28,
     /* identical to box height */
-    
+
     display: 'flex',
     alignItems: 'center',
     textAlign: 'center',
     letterSpacing: -0.3,
-    
+
     /* Function 33 */
-    
-    color: '#2D2F33',    
-  }
+
+    color: '#2D2F33',
+  },
 })
 
 export default App
